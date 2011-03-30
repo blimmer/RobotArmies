e8883dac09bb54eb50a2e16f53cacef4f18548cf
Margaret McNulty <margaret.k.mcnulty@gmail.com>
2011-03-30 14:56:44 -0600

    marketplace

--- src/edu/colorado/RobotArmies/Marketplace.java
+++ src/edu/colorado/RobotArmies/Marketplace.java
@@ -9,9 +9,12 @@ import android.widget.GridView;
 import android.widget.TextView;
 import android.widget.Toast;
 import android.widget.AdapterView.OnItemClickListener;
+import android.content.Intent;
+import android.view.View;
+import android.view.View.OnClickListener;
 
 
-public class Marketplace extends Activity {
+public class Marketplace extends Activity implements OnClickListener {
 	
 	private RobotArmiesDB database = new RobotArmiesDB(this);
 	
@@ -27,15 +30,31 @@ public class Marketplace extends Activity {
         if (c.moveToFirst()) {
             joules = c.getInt(0);
         }
+        
         TextView text1 = (TextView) findViewById(R.id.textView1);
         text1.setText("Welcome to the marketplace! You currently have " + joules + " joules to spend");
         c.close();
         
+        View minionButton = this.findViewById(R.id.minionbutton);
+        minionButton.setOnClickListener(this);
+        
     }
+
+	@Override
+	public void onClick(View v) {
+		switch (v.getId()) {
+		case R.id.minionbutton:
+			Intent i = new Intent(this, BuyRobot.class);
+			startActivity(i);
+			break;
+			// To do the rest of the buttons later
+		}
+		
+	}
     
     
     
-    //TODO: display available options
+    
     //TODO: add logic to check purchasing ability
     //TODO: add communication with robots SQL table
 }
