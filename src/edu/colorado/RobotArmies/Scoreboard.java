25c689b9a0c53ee6baf945fb3dc670b131633bcc
Christopher Schaefbauer <schaefba@colorado.edu>
2011-03-29 23:15:13 -0600

    Basic static scoreboard

--- src/edu/colorado/RobotArmies/Scoreboard.java
+++ src/edu/colorado/RobotArmies/Scoreboard.java
@@ -2,6 +2,8 @@ package edu.colorado.RobotArmies;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.TableLayout;
+import android.widget.TableRow;
 
 public class Scoreboard extends Activity {
     /** Called when the activity is first created. */
@@ -10,6 +12,9 @@ public class Scoreboard extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.scoreboard);
     }
+    
+    
+    
     
     //TODO: backend communication with server to grab stats
 }
