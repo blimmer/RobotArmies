c928f471d7750590599fc65ff39fe4c72cd45256
Margaret McNulty <margaret.k.mcnulty@gmail.com>
2011-03-31 12:48:29 -0600

    Buying minion bot now takes your money ... but doesn't give you a robot. So better not buy a robot. It's a scam I say!

--- src/edu/colorado/RobotArmies/Confirmation.java
+++ src/edu/colorado/RobotArmies/Confirmation.java
@@ -1,6 +1,7 @@
 package edu.colorado.RobotArmies;
 
 import android.app.Activity;
+import android.content.ContentValues;
 import android.content.Intent;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
@@ -9,14 +10,14 @@ import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.TextView;
 
-public class BuyRobot extends Activity implements OnClickListener {
+public class Confirmation extends Activity implements OnClickListener {
 	
 	private RobotArmiesDB database = new RobotArmiesDB(this);
 	private int cost = 100;
 	
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.buy_minion_bot);
+		setContentView(R.layout.confirmation);
 		
         SQLiteDatabase db = database.getWritableDatabase();
         Cursor c = db.query("users", new String[] { "joules" }, null, null, null, null, "_id");
@@ -24,31 +25,27 @@ public class BuyRobot extends Activity implements OnClickListener {
         if (c.moveToFirst()) {
             joules = c.getInt(0);
         }
-                
-        int manyBots = joules/cost;
         
+        joules = joules - cost;
+        ContentValues values = new ContentValues();
+        values.put("joules", joules);
+        db.update("users", values, "username=?", new String[] {Home.name});
+                        
         TextView text1 = (TextView) findViewById(R.id.textView1);
-        TextView text2 = (TextView) findViewById(R.id.textView2);
-        View buyButton = this.findViewById(R.id.buybutton);
-        buyButton.setOnClickListener(this);
+        View backButton = this.findViewById(R.id.backbutton);
+        backButton.setOnClickListener(this);
         
-        text1.setText("The loyal minion bot will do exactly what you tell it to - nothing more, and nothing less." + 
-        	" The minion bot costs " + cost + " joules. You have " + joules + " joules.");
+        text1.setText("Thank you for purchasing a minion bot!" + 
+        	" You now have " + joules + " joules.");
         
-        if (joules < cost) {
-        	text2.setText("Sorry, you don't have enough joules for a minion bot. Perhaps you should go for a nice bike ride and earn some joules!");
-        	buyButton.setClickable(false);
-        } else {
-        	text2.setText("You can afford up to " + manyBots + " minion bots!");
-        }
         c.close();
 	}
 	
 	@Override
 	public void onClick(View v) {
 		switch (v.getId()) {
-		case R.id.buybutton:
-			Intent i = new Intent(this, Confirmation.class);
+		case R.id.backbutton:
+			Intent i = new Intent(this, Marketplace.class);
 			startActivity(i);
 			break;
 			// To do the rest of the buttons later
